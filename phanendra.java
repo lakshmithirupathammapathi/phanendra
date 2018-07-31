8ac622a36410533dd3a29ad839008719c93e37e7
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-07-31 22:23:33 +0530

    Create phanendra.java

---
+++ phanendra.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i,limit=9;
+		System.out.println("the even nums b/w 6 and"+limit);
+		for(i=6;i<=limit;i++)
+		{
+			if(i%2==0)
+			{
+			System.out.println(i+"");	
+			}
+		}
+	}
+}
